22e6994fc1e91e09c6c4236d3ef9d6970465425a
Zainul-jamal <minsarshah1@gmail.com>
2023-11-14 13:22:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function dabaya(a){
+document.getElementById('inp').value+=a
+}
+function eql(){
+   var b = document.getElementById('inp')
+b.value = eval(b.value)
+}
+function clr (){
+document.getElementById('inp').value=''
+}
+function abc(){
+alert("Project BY Hafiz Muhammad Zain-Ul-Abedin")
+}
+alert("Solve Your problems by this Calculator")
